d75685b9ab02462d3c35a8f483b3dfc48c3ec127
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-03 15:30:14 +0300

    updated

--- UILayer/Controllers/ErrorPageController.cs
+++ UILayer/Controllers/ErrorPageController.cs
@@ -3,7 +3,7 @@
 namespace UILayer.Controllers
 { 
     public class ErrorPageController : Controller
-    { 
+    {
         public IActionResult Error404(int code)
         {
             return View(); 
