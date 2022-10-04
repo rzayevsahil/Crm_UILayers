6d69b196a21956c1984737f1e096b9aeef748f56
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-04 20:59:34 +0300

    add view components

---
+++ UILayer/ViewComponents/Dashboard/Chart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.Dashboard
+{
+    public class Chart : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ UILayer/ViewComponents/Dashboard/Widgets.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.Dashboard
+{
+    public class Widgets : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.categoriesCount = context.Categories.Count();
+            ViewBag.productCount = context.Products.Count();
+            return View();
+        }
+    }
+}
