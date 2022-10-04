7772bb18baf765a0f05348194e3da252e20d6f7b
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-04 21:30:23 +0300

    updated

--- UILayer/ViewComponents/Dashboard/Chart.cs
+++ UILayer/ViewComponents/Dashboard/Chart.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UILayer.ViewComponents.Dashboard
 {
     public class Chart : ViewComponent
     {
+        Context context = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.p1 = context.Products.Where(x => x.ProductID == 1).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.p2 = context.Products.Where(x => x.ProductID == 2).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.p3 = context.Products.Where(x => x.ProductID == 3).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.p4 = context.Products.Where(x => x.ProductID == 6).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.p5 = context.Products.Where(x => x.ProductID == 7).Select(y => y.ProductStock).FirstOrDefault();
             return View();
         }
     }
