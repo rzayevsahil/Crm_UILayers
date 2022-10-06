b78ef644b02cc8649cb54ab44d9414c9b0f57f0c
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-06 20:51:49 +0300

    update

---
+++ UILayer/Controllers/RegisterController.cs
@@ -0,0 +1,40 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UILayer.Models;
+
+namespace UILayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(UserSignUpModel userSignUpModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //işlemi yap
+                AppUser user = new AppUser()
+                {
+                    Email = userSignUpModel.Mail,
+                    Name = userSignUpModel.Name,
+                    Surname = userSignUpModel.Surname,
+                    Gender = userSignUpModel.Gender,
+                    UserName = userSignUpModel.Username
+                };
+                //var result= await 
+            }
+            return View();
+        }
+    }
+}
---
+++ UILayer/Models/UserSignUpModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UILayer.Models
+{
+    public class UserSignUpModel
+    {
+        [Required(ErrorMessage = "Lütfen ad değerini boş geçmeyin")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen soyad değerini boş geçmeyin")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen cinsiyet değerini boş geçmeyin")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail değerini boş geçmeyin")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adı değerini boş geçmeyin")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifre değerini boş geçmeyin")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar girin")]
+        [Compare("Password",ErrorMessage = "Şifreler uyuşmuyor lütfen kontrol edin")]
+        public string ConfirmPassword { get; set; }
+    }
+}
