e0fd0af4d254bfc19843883bdafd5c71b843fb48
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-09 09:38:00 +0300

    updared CustomIdentityValidator

--- UILayer/Models/CustomIdentityValidator.cs
+++ UILayer/Models/CustomIdentityValidator.cs
@@ -4,5 +4,39 @@ namespace UILayer.Models
 {
     public class CustomIdentityValidator : IdentityErrorDescriber
     {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Parola minimum {length} karakter olmalıdır"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Şifre en az 1 tane küçük harf içermelidir"
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Şifre en az 1 tane küçük harf içermelidir"
+            };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError()
+            {
+                Code = "DuplicateEmail",
+                Description = $"İlgili mail adresi {email} zaten sistemde kayıtlı, farklı bir mail deneyiniz"
+            }
+        }
     }
 }
