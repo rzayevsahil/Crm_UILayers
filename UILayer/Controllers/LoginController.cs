ccd36fdeb86b6a879b1ce05b43e367664613f69c
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-09 10:22:07 +0300

    updated login index

--- UILayer/Controllers/LoginController.cs
+++ UILayer/Controllers/LoginController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UILayer.Models;
 
 namespace UILayer.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserSignInModel userSignInModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await
+                    _signInManager.PasswordSignInAsync(userSignInModel.UserName, userSignInModel.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
             return View();
         }
     }
---
+++ UILayer/Models/UserSignInModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UILayer.Models
+{
+    public class UserSignInModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adı boş geçmeyin")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string Password { get; set; }
+    }
+}
