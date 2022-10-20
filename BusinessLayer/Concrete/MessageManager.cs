b1792f2736bb0c31fd9f3b740482f5dc444b1fe6
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-20 20:37:50 +0300

    added IMessageService and MessageManager

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal messageDal;
+        public MessageManager(IMessageDal messageDal)
+        {
+            this.messageDal = messageDal;
+        }
+
+        public void TInsert(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
