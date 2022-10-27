e759b59de0b3002040f1f7d860630e1650f90786
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-27 16:05:56 +0300

    updated

---
+++ UILayer/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
