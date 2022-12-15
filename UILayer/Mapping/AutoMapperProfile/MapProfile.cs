77cebfa6a87af976052594f06821cddfcf9c2720
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-12-15 15:26:36 +0300

    updated

---
+++ UILayer/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DTOLayer.DTOs.AnnouncementDtos;
+using DTOLayer.DTOs.AppUserDtos;
+using DTOLayer.DTOs.ProductDtos;
+using EntityLayer.Concrete;
+
+namespace UILayer.Mapping.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Announcement, AnnouncementAddDto>();
+            CreateMap<AnnouncementAddDto, Announcement>();
+
+            CreateMap<Announcement, AnnouncementUpdateDto>();
+            CreateMap<AnnouncementUpdateDto, Announcement>();
+
+            CreateMap<Announcement, AnnouncementListDto>();
+            CreateMap<AnnouncementListDto, Announcement>();
+
+            CreateMap<AppUser, AppUserRegisterDto>();
+            CreateMap<AppUserRegisterDto, AppUser>();
+
+            CreateMap<Product, ProductAddDto>();
+            CreateMap<ProductAddDto, Product>();
+        }
+    }
+}
